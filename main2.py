4ad40e8b6a74e6e8af62956a54bd335d1d7827fc
handrenmuhammad <handrenmuhammad@outlook.com>
2023-07-06 10:39:29 +0300

    showing the mask of first layer

--- main2.py
+++ main2.py
@@ -22,6 +22,39 @@ y_test = to_categorical(y_test)
 model = models.Sequential()
 
 model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1))),
+
+# first_layer_model = models.Model(
+#     inputs=model.input, outputs=model.layers[0].output)
+
+# # Assuming you have an input image 'input_image'
+# # ...
+
+# # Load and preprocess the image
+# image = Image.open('33.jpg')
+# image = image.resize((28, 28))
+# image = image.convert('L')  # Convert to grayscale
+# image = np.array(image) / 255.0  # Normalize pixel values
+
+# # Prepare the image for prediction
+# image = image.reshape(1, 28, 28, 1)  # Reshape and add batch dimension
+
+
+# # Pass the input image through the first layer
+# first_layer_output = first_layer_model.predict(image)
+
+# # Reshape the output to have a shape (height, width, num_filters)
+# first_layer_output = np.squeeze(first_layer_output)
+# height, width, num_filters = first_layer_output.shape
+
+# # Plotting the output of each filter
+# plt.figure(figsize=(10, 6))
+# for i in range(num_filters):
+#     plt.subplot(4, 8, i + 1)  # Assuming 32 filters, adjust as needed
+#     plt.imshow(first_layer_output[:, :, i], cmap='gray')
+#     plt.axis('off')
+# plt.tight_layout()
+# plt.show()
+
 model.add(layers.MaxPooling2D((2, 2))),
 model.add(layers.Conv2D(64, (3, 3), activation='relu')),
 model.add(layers.MaxPooling2D((2, 2))),
